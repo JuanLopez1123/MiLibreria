7b93c024d8c9da17f5b8c00ce5094c79ebbc6a73
unknown <lopezpalominojuand@gmail.com>
2023-08-27 18:07:01 -0500

    Se agrega el proyecto

---
+++ src/PaqueteLibreria/AreaCirculo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PaqueteLibreria;
+
+/**
+ *
+ * @author Juan David Lopez P
+ */
+public class AreaCirculo {
+        //Lo primero que hacemos es crear un metodo el cual se pueda sacar el area de un circulo
+    public static double AreaC(double r){
+        //Se hace la multiplicacion para sacar el area del circulo
+        return (3.14*(r*r));
+    }
+}
---
+++ src/PaqueteLibreria/Hipotenusa.java
@@ -0,0 +1,10 @@
+
+package PaqueteLibreria;
+
+public class Hipotenusa {
+        //Se crea un metodo en el cual se haga la operacion para sacar la hipotenusa
+    public static double Catetos(double a, double b){
+        //Se hace la multiplicacion y la suma para sacar la hipotenusa
+        return ((a*a)+(b*b));
+    }
+}
